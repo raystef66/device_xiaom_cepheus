57fc7d25fda530b8083dfa20b1db5efe7bd237e7
LuK1337 <priv.luk@gmail.com>
2025-02-03 07:19:11 +0100

    cepheus: Shim WFD with libgui_shim/libinput_shim
    Change-Id: I42bf06bc6b19942303031ef0147dafa6df127fe5
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- device.mk
+++ device.mk
@@ -592,6 +592,9 @@ PRODUCT_PACKAGES += \
 
 # WiFi Display
 PRODUCT_PACKAGES += \
+    android.media.audio.common.types-V2-cpp \
+    libgui_shim \
+    libinput_shim \
     libnl \
     libwfdaac_vendor \
     vendor.qti.hardware.display.config-V5-ndk
